9375a6916c9b6fd5767b3cec31264916679c06d3
Jason Sigal <jsigal@spotify.com>
2020-04-27 01:19:32 -0400

    initial prototype

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const http = require('http').createServer(app);
 
-express()
+const io = require('socket.io')(http);
+
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('join-or-create-room', (roomName) => {
+    socket.join(roomName);
+  });
+
+  socket.on('clap', (clap) => {
+    const rooms = Object.keys(socket.rooms);
+    rooms.forEach(room => {
+      socket.to(room).emit('got-clap');
+    });
+  });
+
+});
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/*', (req, res) => res.render('pages/room'))
+
+http
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
